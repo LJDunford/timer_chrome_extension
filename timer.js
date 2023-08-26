25fa843f92c5c646c23c3e1ac95f0baf10a3a27d
Lewis Dunford <Lewis@Lewis-MacBook.local>
2023-08-26 16:54:35 +0100

    initial commit - working timer

---
+++ timer.js
@@ -0,0 +1,59 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    const startButton = document.getElementById('startButton');
+    const hoursInput = document.getElementById('hours');
+    const minutesInput = document.getElementById('minutes');
+    const timerDisplay = document.getElementById('timer');
+    const pauseButton = document.getElementById('pauseButton');
+    const endButton = document.getElementById('endButton');
+
+    let interval;
+
+    if (startButton) {
+        startButton.addEventListener('click', () => {
+            const hours = parseInt(hoursInput.value, 10);
+            const minutes = parseInt(minutesInput.value, 10);
+      
+            startCountdown(hours, minutes);
+        })
+    }
+     
+    if (pauseButton) {
+        pauseButton.addEventListener('click', pauseCountdown());
+    }
+    
+    if (endButton) {
+        endButton.addEventListener('click', endCountdown());
+    }
+
+    // ***** FUNCTIONS *****
+    function startCountdown(hours, minutes) {
+        let totalSeconds = (hours * 3600) + (minutes * 60);
+  
+        const interval = setInterval(() => {
+            if (totalSeconds <= 0) {
+                clearInterval(interval);
+                timerDisplay.innerHTML = "Time's up! Go take a well deserved break!";
+                return;
+            }
+  
+            const hoursRemaining = Math.floor(totalSeconds / 3600);
+            const minutesRemaining = Math.floor((totalSeconds % 3600) / 60);
+            const secondsRemaining = totalSeconds % 60;
+  
+            const formattedTime = `${hoursRemaining.toString().padStart(2, '0')}:${minutesRemaining.toString().padStart(2, '0')}:${secondsRemaining.toString().padStart(2, '0')}`;
+            timerDisplay.innerHTML = formattedTime;
+  
+            totalSeconds--;
+        }, 1000);
+    }
+
+    function pauseCountdown() {
+        clearInterval(interval);
+    }
+  
+    function endCountdown() {
+        clearInterval(interval);
+        timerDisplay.innerHTML = "Countdown ended.";
+    }
+});
